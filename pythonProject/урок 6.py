bde04035a54c7d427f29169e64e998e0af8f866a
taysikret <115655963+taysikret@users.noreply.github.com>
2022-11-08 20:16:59 +0300

    урок 5

---
+++ pythonProject/урок 6.py
@@ -0,0 +1,10 @@
+
+# data = "Case study"
+# for i in data:
+#     print(i *2)
+# else:
+#     print(f"Последний символ: {i}. Цикл завершен");
+# print("Работа программы завершена")
+
+for i in range (6):
+    print(i)
